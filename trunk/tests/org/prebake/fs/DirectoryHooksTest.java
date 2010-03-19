a894d0e5e97277c7a08b9aef9634a4e9c643d7c0
mikesamuel <mikesamuel@5c2138e2-2bf6-11df-8484-2f92f965d510>
2010-03-19 23:21:50 +0000

    got DirectoryHooks tests running on default file system

--- trunk/tests/org/prebake/fs/DirectoryHooksTest.java
+++ trunk/tests/org/prebake/fs/DirectoryHooksTest.java
@@ -1,11 +1,23 @@
 package org.prebake.fs;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.URI;
 import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 
@@ -14,58 +26,144 @@ import org.prebake.service.StubFileSystemProvider;
 import junit.framework.TestCase;
 
 public class DirectoryHooksTest extends TestCase {
-  public final void testHooks() throws Exception {
-    int delay = 100;
-    TimeUnit ms = TimeUnit.MILLISECONDS;
 
+  private static final FileAttribute<?>[] DIR_ATTRS = new FileAttribute<?>[] {
+    PosixFilePermissions.asFileAttribute(
+        PosixFilePermissions.fromString("rwx------"))
+  };
+
+  private static final FileAttribute<?>[] FILE_ATTRS = new FileAttribute<?>[] {
+    PosixFilePermissions.asFileAttribute(
+        PosixFilePermissions.fromString("rw-------"))
+  };
+
+  public final void testOnStubFileSystem() throws Exception {
+    int delay = 100;
     FileSystem fs = new StubFileSystemProvider("mfs")
         .getFileSystem(URI.create("mfs://#/foo/bar"));
-    Path p = fs.getPath("/foo/bar");
-    fs.getPath("/foo/bar/baz").createDirectory();
-    fs.getPath("/foo/bar/baz/boo.cc").createFile();
-    fs.getPath("/foo/bar/baz/boo.h").createFile();
-    fs.getPath("/foo/bar/README").createFile();
-    DirectoryHooks dh = new DirectoryHooks(p);
+    fs.getPath("/foo").createDirectory();
+    Path dir = fs.getPath("/foo/bar");
+    dir.createDirectory();
+    runTests(delay, fs, dir);
+  }
+
+  public final void testOnDefaultFileSystem() throws Exception {
+    int delay = 100;
+    FileSystem fs = FileSystems.getDefault();
+    Path dir = fs.getPath("" + File.createTempFile("temp", "dir"));
+    try {
+      dir.delete();
+      if (dir.notExists()) {
+        dir.createDirectory(DIR_ATTRS);
+      }
+      runTests(delay, fs, fs.getPath(dir.toString()));
+    } finally {
+      rmTree(dir);
+    }
+  }
+
+  private void runTests(int delay, FileSystem fs, Path dir)
+      throws IOException, InterruptedException {
+    Path baz = dir.resolve(fs.getPath("baz"));
+    dir.resolve(fs.getPath("README")).createFile(FILE_ATTRS);
+    baz.createDirectory(DIR_ATTRS);
+    baz.resolve(fs.getPath("boo.cc")).createFile(FILE_ATTRS);
+    baz.resolve(fs.getPath("boo.h")).createFile(FILE_ATTRS);
+    DirectoryHooks dh = new DirectoryHooks(dir);
     dh.start();
     BlockingQueue<Path> q = dh.getUpdates();
-    assertEquals("/foo/bar/baz/boo.cc", "" + poll(q, delay, ms));
-    assertEquals("/foo/bar/baz/boo.h", "" + poll(q, delay, ms));
-    assertEquals("/foo/bar/README", "" + poll(q, delay, ms));
-    assertNull(poll(q, delay, ms));
+
+    assertChanged(
+        q, delay,
+        dir.resolve(fs.getPath("README")),
+        baz.resolve(fs.getPath("boo.cc")),
+        baz.resolve(fs.getPath("boo.h")));
 
     // Create a file
-    fs.getPath("/foo/bar/baz/boo2.cc").createFile();
-    assertEquals("/foo/bar/baz/boo2.cc", "" + poll(q, delay, ms));
-    assertNull(poll(q, delay, ms));
+    Path boo2cc = baz.resolve(fs.getPath("boo2.cc"));
+    boo2cc.createFile(FILE_ATTRS);
+    assertChanged(q, delay, boo2cc);
 
     // Modify a file
-    OutputStream out = fs.getPath("/foo/bar/baz/boo.cc").newOutputStream();
+    Path boocc = baz.resolve(fs.getPath("boo.cc"));
+    OutputStream out = boocc.newOutputStream();
     Writer w = new OutputStreamWriter(out, "UTF-8");
     w.write("\nprintf(\"Hello, World!\\n\");\n");
     w.close();
-    assertEquals("/foo/bar/baz/boo.cc", "" + poll(q, delay, ms));
-    assertNull(poll(q, delay, ms));
+    assertChanged(q, delay, boocc);
 
     // Delete a file
-    fs.getPath("/foo/bar/README").deleteIfExists();
-    fs.getPath("/foo/bar/LICENSE").deleteIfExists();  // does not exist
-    assertEquals("/foo/bar/README", "" + poll(q, delay, ms));
-    assertNull(poll(q, delay, ms));
+    Path README = dir.resolve(fs.getPath("README"));
+    Path LICENSE = dir.resolve(fs.getPath("LICENSE"));
+    README.deleteIfExists();
+    LICENSE.deleteIfExists();  // does not exist
+    assertChanged(q, delay, README);
 
     // Create a directory and put a file in it.
-    fs.getPath("/foo/bar/noo").createDirectory();
-    fs.getPath("/foo/bar/noo/foo.txt").createFile();
-    assertEquals("/foo/bar/noo/foo.txt", "" + poll(q, delay, ms));
-    assertEquals(null, poll(q, delay, ms));
+    Path noo = dir.resolve(fs.getPath("noo"));
+    noo.createDirectory(DIR_ATTRS);
+    Path footxt = noo.resolve(fs.getPath("foo.txt"));
+    footxt.createFile(FILE_ATTRS);
+    assertChanged(q, delay, footxt);
 
     // Recreate the deleted file
-    fs.getPath("/foo/bar/README").createFile();
-    assertEquals("/foo/bar/README", "" + poll(q, delay, ms));
-    assertEquals(null, poll(q, delay, ms));
+    README.createFile(FILE_ATTRS);
+    assertChanged(q, delay, README);
   }
 
-  private static <T> T poll(BlockingQueue<T> q, int delay, TimeUnit u)
+  private static void assertChanged(
+      BlockingQueue<Path> q, int delayMillis, Path... expected)
       throws InterruptedException {
-    return q.poll(delay, u);
+    Set<Path> golden = new HashSet<Path>(Arrays.asList(expected));
+    Set<Path> actual = new HashSet<Path>();
+    long t = System.currentTimeMillis();
+    long te = t + delayMillis;
+    do {
+      Path p = q.poll(Math.max(1, te - t), TimeUnit.MILLISECONDS);
+      if (p != null) {
+        actual.add(p);
+        q.drainTo(actual);
+      }
+      t = System.currentTimeMillis();
+    } while (t < te);
+    assertEquals(golden, actual);
+  }
+
+  private static void rmTree(Path p) {
+    Files.walkFileTree(p, new FileVisitor<Path>() {
+      public FileVisitResult postVisitDirectory(Path p, IOException ex) {
+        if (ex != null) { ex.printStackTrace(); }
+        try {
+          p.delete();
+        } catch (IOException ex2) {
+          ex2.printStackTrace();
+        }
+        return FileVisitResult.CONTINUE;
+      }
+
+      public FileVisitResult preVisitDirectory(Path p) {
+        return FileVisitResult.CONTINUE;
+      }
+
+      public FileVisitResult preVisitDirectoryFailed(Path p, IOException ex) {
+        return FileVisitResult.CONTINUE;
+      }
+
+      public FileVisitResult visitFile(Path p, BasicFileAttributes attrs) {
+        if (!attrs.isDirectory()) {
+          try {
+            p.deleteIfExists();
+          } catch (IOException ex) {
+            ex.printStackTrace();
+          }
+        }
+        return FileVisitResult.CONTINUE;
+      }
+
+      public FileVisitResult visitFileFailed(Path p, IOException ex) {
+        ex.printStackTrace();
+        return FileVisitResult.CONTINUE;
+      }
+    });
   }
 }
