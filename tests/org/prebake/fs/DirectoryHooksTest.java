8fa2d940e993b8a8119ce7cca22cdfa0e7a0fb10
mikesamuel <mikesamuel@5c2138e2-2bf6-11df-8484-2f92f965d510>
2010-03-19 22:11:32 +0000

    more dir hooks tests

--- tests/org/prebake/fs/DirectoryHooksTest.java
+++ tests/org/prebake/fs/DirectoryHooksTest.java
@@ -1,5 +1,8 @@
 package org.prebake.fs;
 
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.URI;
 import java.nio.file.FileSystem;
 import java.nio.file.Path;
@@ -11,7 +14,10 @@ import org.prebake.service.StubFileSystemProvider;
 import junit.framework.TestCase;
 
 public class DirectoryHooksTest extends TestCase {
-  public final void testInit() throws Exception {
+  public final void testHooks() throws Exception {
+    int delay = 100;
+    TimeUnit ms = TimeUnit.MILLISECONDS;
+
     FileSystem fs = new StubFileSystemProvider("mfs")
         .getFileSystem(URI.create("mfs://#/foo/bar"));
     Path p = fs.getPath("/foo/bar");
@@ -22,12 +28,44 @@ public class DirectoryHooksTest extends TestCase {
     DirectoryHooks dh = new DirectoryHooks(p);
     dh.start();
     BlockingQueue<Path> q = dh.getUpdates();
-    assertEquals(
-        "/foo/bar/baz/boo.cc", q.poll(500, TimeUnit.MILLISECONDS).toString());
-    assertEquals(
-        "/foo/bar/baz/boo.h", q.poll(500, TimeUnit.MILLISECONDS).toString());
-    assertEquals(
-        "/foo/bar/README", q.poll(500, TimeUnit.MILLISECONDS).toString());
-    assertNull(q.poll(500, TimeUnit.MILLISECONDS));
+    assertEquals("/foo/bar/baz/boo.cc", "" + poll(q, delay, ms));
+    assertEquals("/foo/bar/baz/boo.h", "" + poll(q, delay, ms));
+    assertEquals("/foo/bar/README", "" + poll(q, delay, ms));
+    assertNull(poll(q, delay, ms));
+
+    // Create a file
+    fs.getPath("/foo/bar/baz/boo2.cc").createFile();
+    assertEquals("/foo/bar/baz/boo2.cc", "" + poll(q, delay, ms));
+    assertNull(poll(q, delay, ms));
+
+    // Modify a file
+    OutputStream out = fs.getPath("/foo/bar/baz/boo.cc").newOutputStream();
+    Writer w = new OutputStreamWriter(out, "UTF-8");
+    w.write("\nprintf(\"Hello, World!\\n\");\n");
+    w.close();
+    assertEquals("/foo/bar/baz/boo.cc", "" + poll(q, delay, ms));
+    assertNull(poll(q, delay, ms));
+
+    // Delete a file
+    fs.getPath("/foo/bar/README").deleteIfExists();
+    fs.getPath("/foo/bar/LICENSE").deleteIfExists();  // does not exist
+    assertEquals("/foo/bar/README", "" + poll(q, delay, ms));
+    assertNull(poll(q, delay, ms));
+
+    // Create a directory and put a file in it.
+    fs.getPath("/foo/bar/noo").createDirectory();
+    fs.getPath("/foo/bar/noo/foo.txt").createFile();
+    assertEquals("/foo/bar/noo/foo.txt", "" + poll(q, delay, ms));
+    assertEquals(null, poll(q, delay, ms));
+
+    // Recreate the deleted file
+    fs.getPath("/foo/bar/README").createFile();
+    assertEquals("/foo/bar/README", "" + poll(q, delay, ms));
+    assertEquals(null, poll(q, delay, ms));
+  }
+
+  private static <T> T poll(BlockingQueue<T> q, int delay, TimeUnit u)
+      throws InterruptedException {
+    return q.poll(delay, u);
   }
 }
