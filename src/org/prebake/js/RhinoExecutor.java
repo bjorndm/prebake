e8dea69847fbdaf13659549769c746c1485f4281
mikesamuel <mikesamuel@5c2138e2-2bf6-11df-8484-2f92f965d510>
2010-03-22 22:40:59 +0000

    flesh out JS console impl

--- src/org/prebake/js/RhinoExecutor.java
+++ src/org/prebake/js/RhinoExecutor.java
@@ -5,13 +5,20 @@ import java.io.IOException;
 import java.io.Reader;
 import java.net.URI;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Formatter;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.mozilla.javascript.ClassShutter;
 import org.mozilla.javascript.Context;
@@ -155,6 +162,8 @@ public final class RhinoExecutor implements Executor {
         case Context.FEATURE_E4X: return false;
         case Context.FEATURE_ENHANCED_JAVA_ACCESS: return false;
         case Context.FEATURE_PARENT_PROTO_PROPERTIES: return false;
+        case Context.FEATURE_STRICT_VARS: return true;
+        case Context.FEATURE_STRICT_EVAL: return true;
         default: return super.hasFeature(c, feature);
       }
     }
@@ -219,8 +228,7 @@ public final class RhinoExecutor implements Executor {
     }
     try {
       globalScope.defineProperty(
-          "console", new Console(context, globalScope, logger),
-          ScriptableObject.DONTENUM);
+          "console", new Console(logger), ScriptableObject.DONTENUM);
       for (Map.Entry<String, ?> e : actuals.entrySet()) {
         globalScope.defineProperty(
             e.getKey(), Context.javaToJS(e.getValue(), globalScope),
@@ -265,19 +273,54 @@ public final class RhinoExecutor implements Executor {
     }
   }
 
-  public static class Console {
-    private final Context cx;
-    private final Scriptable global;
-    private final Logger logger;
+  private static final Pattern STACK_FRAME = Pattern.compile(
+      "^\tat ([^:]+):(\\d+)(?: \\(([^)]+)\\))?", Pattern.MULTILINE);
 
-    Console(Context cx, Scriptable global, Logger logger) {
-      this.cx = cx;
-      this.global = global;
+  public static class Console {
+    private final Logger logger;
+    private final List<Group> groups = new ArrayList<Group>();
+    private final Map<String, Long> timers = new HashMap<String, Long>();
+
+    Console(Logger logger) {
       this.logger = logger;
     }
 
     private void log(Level level, String format, Object... args) {
-      logger.log(level, String.format(format, args));
+      for (int i = args.length; --i >= 0;) {
+        Object o = args[i];
+        if (o instanceof Double
+            && ((Double) o).doubleValue() == ((Double) o).longValue()) {
+          // Convert Doubles to Longs when doing so does not lose information.
+          // This solves the problem of console.log("%s", 1) logging "1.0",
+          // and fixes "%d".
+          args = args.clone();
+          args[i] = Long.valueOf(((Double) o).longValue());
+          while (--i >= 0) {
+            o = args[i];
+            if (o instanceof Double
+                && ((Double) o).doubleValue() == ((Double) o).longValue()) {
+              args[i] = Long.valueOf(((Double) o).longValue());
+            }
+          }
+        }
+      }
+      StringBuilder sb = new StringBuilder();
+      int nSpaces = groups.size() * 2;
+      while (nSpaces >= 16) { sb.append(SIXTEEN_SPACES); nSpaces -= 16; }
+      sb.append(SIXTEEN_SPACES, 0, nSpaces);
+      Formatter f = new Formatter(sb /*, default Locale */);
+      f.format(format, args);
+      LogRecord lr = new LogRecord(level, sb.toString());
+      Matcher m = STACK_FRAME.matcher(
+          new EvaluatorException(null).getScriptStackTrace());
+      if (m.find()) {
+        String file = m.group(1);
+        String line = m.group(2);
+        String fnName = m.group(3);
+        lr.setSourceClassName(file + ":" + line);
+        lr.setSourceMethodName(fnName != null ? fnName : "<anonymous>");
+      }
+      logger.log(lr);
     }
 
     public void log(String format, Object... args) {
@@ -296,7 +339,119 @@ public final class RhinoExecutor implements Executor {
       log(Level.FINE, format, args);
     }
 
-    // TODO: provide the rest of the Firebug console API
+    public void debug(String format, Object... args) {
+      log(Level.INFO, format, args);
+    }
+
+    public void assert_(Object truth) { assert_(truth, null); }
+
+    public void assert_(Object truth, Object message) {
+      if (!Context.toBoolean(truth)) {
+        if (message == null) { message = "Assertion Failure"; }
+        String messageStr = message.toString();
+        error(messageStr);
+        throw new JavaScriptException(messageStr, "console", 1);
+      }
+    }
+
+    public void dir(Object obj) {
+      List<String> pairs = new ArrayList<String>();
+      pairs.add("Name");
+      pairs.add("Value");
+      if (obj instanceof Scriptable) {
+        Scriptable s = (Scriptable) obj;
+        for (Object id : s.getIds()) {
+          if (id instanceof Number) {
+            pairs.add("" + id);
+            pairs.add(valueToStr(s.get(((Number) id).intValue(), s)));
+          } else {
+            String idStr = (String) id;
+            pairs.add(idStr);
+            pairs.add(valueToStr(s.get(idStr, s)));
+          }
+        }
+      }
+      log(Level.INFO, toTable(pairs, 2));
+    }
+
+    public void group(String name) {
+      log(Level.INFO, "Enter " + name);
+      groups.add(new Group(name));
+    }
+
+    public void groupEnd() {
+      log(Level.INFO, "Exit  " + groups.remove(0).name);
+    }
+
+    public void time(String name) {
+      timers.put(name, System.nanoTime());
+    }
+
+    public void timeEnd(String name) {
+      Long t0 = timers.remove(name);
+      if (t0 != null) {
+        long t1 = System.nanoTime();
+        log(Level.INFO, "Timer %s took %sns", name, t1 - t0);
+      }
+    }
+
+    public void trace() {
+      log(Level.INFO, new EvaluatorException(null).getScriptStackTrace());
+    }
+
+    // TODO: profile, profileEnd, count
+
+    private static final String SIXTEEN_SPACES = "                ";
+    static { assert 16 == SIXTEEN_SPACES.length(); }
+
+    private static String valueToStr(Object o) {
+      if (o == null) { return "null"; }
+      if (o.getClass().isArray()) {
+        if (o instanceof Object[]) { return Arrays.toString((Object[]) o); }
+        Class<?> cl = o.getClass().getComponentType();
+        if (cl == Byte.TYPE) { return Arrays.toString((byte[]) o); }
+        if (cl == Character.TYPE) { return Arrays.toString((char[]) o); }
+        if (cl == Double.TYPE) { return Arrays.toString((double[]) o); }
+        if (cl == Float.TYPE) { return Arrays.toString((float[]) o); }
+        if (cl == Integer.TYPE) { return Arrays.toString((int[]) o); }
+        if (cl == Long.TYPE) { return Arrays.toString((long[]) o); }
+        if (cl == Short.TYPE) { return Arrays.toString((short[]) o); }
+      }
+      return Context.toString(o);
+    }
+
+    private static String toTable(List<? extends String> cells, int n) {
+      int m = cells.size() / n;
+      int[] maxLens = new int[n];
+      for (int i = 0; i < m * n; ++i) {
+        maxLens[i % n] = Math.max(maxLens[i % n], cells.get(i).length());
+      }
+      int rowLen = 3 * n;  // n + 1 dividers, 2(n - 1) spaces, and 1 newline.
+      for (int len : maxLens) { rowLen += len; }
+      StringBuilder sb = new StringBuilder(rowLen * m);
+      for (int k = 0; k < m * n;) {
+        sb.append("\n|");
+        for (int j = 0; j < n; ++j, ++k) {
+          sb.append(' ');
+          String s = cells.get(k);
+          sb.append(s);
+          int padding = maxLens[j] - s.length();
+          while (padding >= 16) {
+            sb.append(SIXTEEN_SPACES);
+            padding -= 16;
+          }
+          sb.append(SIXTEEN_SPACES, 0, padding);
+          sb.append(" |");
+        }
+      }
+      return sb.toString();
+    }
+  }
+
+  private static final class Group {
+    final String name;
+
+    Group(String name) { this.name = name; }
   }
 
   public static class LoadFn extends ScriptableObject implements Function {
