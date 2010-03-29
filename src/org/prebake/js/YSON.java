30357382f6c63643bf2ce6dd38a0e1f512e875d2
mikesamuel <mikesamuel@5c2138e2-2bf6-11df-8484-2f92f965d510>
2010-03-29 02:39:50 +0000

    implemented YSON

---
+++ src/org/prebake/js/YSON.java
@@ -0,0 +1,450 @@
+package org.prebake.js;
+
+import org.prebake.core.MessageQueue;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import java.text.ParseException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import org.mozilla.javascript.EvaluatorException;
+import org.mozilla.javascript.Node;
+import org.mozilla.javascript.Parser;
+import org.mozilla.javascript.Token;
+import org.mozilla.javascript.ast.ArrayLiteral;
+import org.mozilla.javascript.ast.AstNode;
+import org.mozilla.javascript.ast.CatchClause;
+import org.mozilla.javascript.ast.ExpressionStatement;
+import org.mozilla.javascript.ast.FunctionNode;
+import org.mozilla.javascript.ast.KeywordLiteral;
+import org.mozilla.javascript.ast.Name;
+import org.mozilla.javascript.ast.NodeVisitor;
+import org.mozilla.javascript.ast.NumberLiteral;
+import org.mozilla.javascript.ast.ObjectLiteral;
+import org.mozilla.javascript.ast.ObjectProperty;
+import org.mozilla.javascript.ast.ParenthesizedExpression;
+import org.mozilla.javascript.ast.ScriptNode;
+import org.mozilla.javascript.ast.StringLiteral;
+import org.mozilla.javascript.ast.UnaryExpression;
+import org.mozilla.javascript.ast.VariableDeclaration;
+import org.mozilla.javascript.ast.VariableInitializer;
+
+/**
+ * Extends JSON to allow for functions with no free variables.
+ * The letter Y is a crude transliteration of &lambda; into the Roman alphabet.
+ *
+ * @author mikesamuel@gmail.com
+ */
+public final class YSON {
+  private final AstNode root;
+  private YSON(AstNode root) { this.root = root; }
+
+  public Set<String> getFreeNames() {
+    // The free names of a production P are union(RN(P), LSD(P), VSD(P))
+    Set<String> fn = Sets.newLinkedHashSet();
+    rn(root, fn);
+    vsd(root, fn);
+    return fn;
+  }
+
+  Set<String> getRequiredNames() {
+    Set<String> rn = Sets.newLinkedHashSet();
+    rn(root, rn);
+    return rn;
+  }
+
+  Set<String> getVarScopeDeclarations() {
+    Set<String> vsd = Sets.newLinkedHashSet();
+    vsd(root, vsd);
+    return vsd;
+  }
+
+  public String toSource() { return root.toSource(); }
+
+  public Object toJavaObject() {
+    return YSON.toJavaObject(root);
+  }
+
+  public static final Set<String> DEFAULT_YSON_ALLOWED = ImmutableSet.of(
+      "Array",
+      "Boolean",
+      "Date",
+      "Error",
+      "EvalError",
+      "Function",
+      "Infinity",
+      "JSON",
+      "Math",
+      "NaN",
+      "Number",
+      "Object",
+      "RangeError",
+      "ReferenceError",
+      "RegExp",
+      "String",
+      "SyntaxError",
+      "TypeError",
+      "URIError",
+      "decodeURIComponent",
+      "encodeURIComponent",
+      "eval",
+      "isFinite",
+      "isNaN",
+      "parseFloat",
+      "parseInt",
+      "undefined"
+      );
+
+  public static boolean isYSON(
+      String js, Set<String> allowedFreeVars, MessageQueue mq) {
+    return requireYSON(js, allowedFreeVars, mq) != null;
+  }
+
+  public static YSON requireYSON(
+      String js, Set<String> allowedFreeVars, MessageQueue mq) {
+    if (js == null) {
+      if (mq != null) { mq.getMessages().add("Output is null"); }
+      return null;
+    }
+    YSON yson;
+    try {
+      yson = parseExpr(js);
+    } catch (ParseException ex) {
+      if (mq != null) { mq.getMessages().add(ex.getMessage()); }
+      return null;
+    }
+
+    return requireYSON(yson, allowedFreeVars, mq);
+  }
+
+  public static YSON requireYSON(
+      YSON yson, Set<String> allowedFreeVars, MessageQueue mq) {
+    Set<String> freeNames = yson.getFreeNames();
+    freeNames.removeAll(allowedFreeVars);
+    if (!freeNames.isEmpty()) {
+      if (mq != null) {
+        mq.getMessages().add(
+            "Disallowed free variables: " + Joiner.on(", ").join(freeNames));
+      }
+      return null;
+    }
+    return isStructuralYSON(yson.root, mq) ? yson : null;
+  }
+
+  private static boolean isStructuralYSON(
+      final AstNode node, final MessageQueue mq) {
+    switch (node.getType()) {
+      case Token.FALSE: case Token.FUNCTION: case Token.NULL:
+      case Token.NUMBER: case Token.STRING: case Token.TRUE:
+      case Token.NEG: case Token.POS:
+        return true;
+      case Token.ARRAYLIT: case Token.EXPR_RESULT: case Token.LP:
+      case Token.OBJECTLIT: case Token.SCRIPT:
+        final boolean[] result = new boolean[] { true };
+        node.visit(new NodeVisitor() {
+          public boolean visit(AstNode n) {
+            if (n == node) { return true; }
+            if (!isStructuralYSON(n, mq)) { result[0] = false; }
+            return false;
+          }
+        });
+        return result[0];
+      case Token.COLON:
+        return isStructuralYSON(((ObjectProperty) node).getRight(), mq);
+      case Token.EMPTY:
+        if (mq != null) {
+          mq.getMessages().add("Not YSON: trailing commas or empty expression");
+        }
+        return false;
+      default:
+        if (mq != null) {
+          mq.getMessages().add("Not YSON: " + node.toSource());
+        }
+        return false;
+    }
+  }
+
+  public static YSON parse(String js) throws ParseException {
+    try {
+      return new YSON(new Parser().parse(js, "YSON", 1));
+    } catch (EvaluatorException ex) {
+      throw new ParseException(ex.details() + " in " + abbrev(js), -1);
+    }
+  }
+
+  private static String abbrev(String s) {
+    return (s.length() < 40 ? s : s.substring(0, 37) + "...")
+        .replaceAll("[\r\n]+", " ");
+  }
+
+  private static final Pattern AMBIGUOUS_START = Pattern.compile(
+      ""
+      + "^(?:"
+        // line continuation
+        + "\\\\(?:\r\n?|[\n\u2028\u2029])"
+        // whitespace
+        + "|[\\ufeff\\s]+"
+        // line comment
+        + "|//[^\r\n\u2028\u2029]"
+        // block comment
+        + "|/\\*.*?\\*/"
+      + ")*"
+      // The keyword function or an open curly brace
+      + "(?:\\{|function[^\\w$_])");
+  public static YSON parseExpr(String js) throws ParseException {
+    // If the input could be confused with a block or function declaration,
+    // parenthesize it.  Don't always do this because that could let some
+    // illegal inputs through (e.g. "1) + (1", and produces more confusing
+    // error messages.
+    // TokenStream not public so figure out whether it starts with an ambiguous
+    // production using a regex.
+    if (AMBIGUOUS_START.matcher(js).find()) { js = "(" + js + ")"; }
+    YSON yson = parse(js);
+    if (yson.root instanceof ScriptNode) {
+      ScriptNode n = (ScriptNode) yson.root;
+      Iterator<Node> children = n.iterator();
+      if (children.hasNext()) {
+        Node child = children.next();
+        if (!children.hasNext()) {
+          if (child instanceof ExpressionStatement) {
+            child = ((ExpressionStatement) child).getExpression();
+            return new YSON((AstNode) child);
+          }
+        }
+      }
+    }
+    return yson;
+  }
+
+  public static YSON filterProperties(YSON yson, Predicate<String> allow) {
+    filterProperties(yson.root, allow);
+    return yson;
+  }
+
+  private static void filterProperties(AstNode ast, Predicate<String> allow) {
+    while (true) {
+      switch (ast.getType()) {
+        case Token.SCRIPT:
+          for (Node child : (ScriptNode) ast) {
+            filterProperties((AstNode) child, allow);
+          }
+          return;
+        case Token.EXPR_RESULT:
+          ast = ((ExpressionStatement) ast).getExpression();
+          continue;
+        case Token.LP:
+          ast = ((ParenthesizedExpression) ast).getExpression();
+          continue;
+        case Token.OBJECTLIT:
+          ObjectLiteral obj = (ObjectLiteral) ast;
+          for (ObjectProperty prop: ImmutableList.copyOf(obj.getElements())) {
+            boolean remove;
+            if (prop.isGetter() || prop.isSetter()) {
+              remove = true;
+            } else {
+              remove = !allow.apply(propertyKeyToString(prop.getLeft()));
+            }
+            if (remove) { obj.getElements().remove(prop); }
+          }
+          break;
+      }
+      break;
+    }
+  }
+
+  private static String propertyKeyToString(AstNode key) {
+    if (key instanceof Name) {
+      return ((Name) key).getIdentifier();
+    } else if (key instanceof StringLiteral) {
+      return ((StringLiteral) key).getValue();
+    } else {
+      return key.toSource(0);
+    }
+  }
+
+  private static void rn(AstNode node, Set<String> names) {
+    switch (node.getType()) {
+      case Token.BREAK: case Token.CONTINUE: break;
+      case Token.CATCH: {
+        CatchClause cc = (CatchClause) node;
+        Set<String> rnBody = Sets.newHashSet();
+        rn(cc.getBody(), rnBody);
+        rnBody.remove(cc.getVarName().getIdentifier());
+        names.addAll(rnBody);
+        return;
+      }
+      case Token.CONST: case Token.VAR:
+        for (VariableInitializer v
+             : ((VariableDeclaration) node).getVariables()) {
+          AstNode init = v.getInitializer();
+          if (init != null) { rn(init, names); }
+        }
+        return;
+      case Token.FUNCTION: {
+        Set<String> declared = Sets.newHashSet();
+        declared.add("this");
+        declared.add("arguments");
+        FunctionNode fn = (FunctionNode) node;
+        Name name = fn.getFunctionName();
+        if (name != null) {
+          declared.add(name.getIdentifier());
+        }
+        for (AstNode param : fn.getParams()) {
+          declared.add(((Name) param).getIdentifier());
+        }
+        vsdAll(fn.getBody(), declared);
+        Set<String> rnBody = Sets.newLinkedHashSet();
+        rnAll(fn.getBody(), rnBody);
+        rnBody.removeAll(declared);
+        names.addAll(rnBody);
+        return;
+      }
+      case Token.COLON:
+        if (node instanceof ObjectProperty) {
+          rn(((ObjectProperty) node).getRight(), names);
+        }
+        break;
+      case Token.NAME: names.add(((Name) node).getIdentifier()); break;
+      case Token.THIS: names.add("this"); break;
+      default: rnAll(node, names); break;
+    }
+  }
+
+  private static void rnAll(final AstNode node, final Set<String> names) {
+    node.visit(new NodeVisitor() {
+      public boolean visit(AstNode n) {
+        if (n == node) { return true; }
+        rn(n, names);
+        return false;
+      }
+    });
+  }
+
+  private static void vsd(AstNode node, Set<String> names) {
+    switch (node.getType()) {
+      case Token.BREAK: case Token.CONTINUE:
+      case Token.EXPR_RESULT: case Token.EXPR_VOID:
+        break;
+      case Token.CONST: case Token.VAR:
+        for (VariableInitializer v
+             : ((VariableDeclaration) node).getVariables()) {
+          names.add(((Name) v.getTarget()).getIdentifier());
+        }
+        break;
+      case Token.FUNCTION:
+        FunctionNode fn = (FunctionNode) node;
+        Name name = fn.getFunctionName();
+        if (name != null && name.getDefiningScope() != node) {
+          names.add(name.getIdentifier());
+        }
+        break;
+      default:
+        vsdAll(node, names);
+        break;
+    }
+  }
+
+  private static void vsdAll(final AstNode node, final Set<String> names) {
+    node.visit(new NodeVisitor() {
+      public boolean visit(AstNode n) {
+        if (n == node) { return true; }
+        vsd(n, names);
+        return false;
+      }
+    });
+  }
+
+  private static Object toJavaObject(AstNode node) {
+    if (node instanceof StringLiteral) {
+      return ((StringLiteral) node).getValue();
+    } else if (node instanceof NumberLiteral) {
+      return ((NumberLiteral) node).getDouble();
+    } else if (node instanceof FunctionNode) {
+      return new Lambda(node.toSource());
+    } else if (node instanceof KeywordLiteral) {
+      switch (((KeywordLiteral) node).getType()) {
+        case Token.TRUE: return Boolean.TRUE;
+        case Token.FALSE: return Boolean.FALSE;
+        default: return null;
+      }
+    } else if (node instanceof ObjectLiteral) {
+      Map<String, Object> m = Maps.newLinkedHashMap();
+      boolean hasNull = false;
+      for (ObjectProperty prop: ((ObjectLiteral) node).getElements()) {
+        String key = propertyKeyToString(prop.getLeft());
+        Object value = toJavaObject(prop.getRight());
+        m.put(key, value);
+        if (value == null) { hasNull = true; }
+      }
+      return hasNull ? Collections.unmodifiableMap(m) : ImmutableMap.copyOf(m);
+    } else if (node instanceof ArrayLiteral) {
+      List<Object> els = Lists.newArrayList();
+      for (AstNode el : ((ArrayLiteral) node).getElements()) {
+        els.add(toJavaObject(el));
+      }
+      return els.contains(null)
+          ? Collections.unmodifiableList(els) : ImmutableList.copyOf(els);
+    } else if (node instanceof ScriptNode) {
+      Iterator<Node> children = ((ScriptNode) node).iterator();
+      if (children.hasNext()) {
+        return toJavaObject((AstNode) children.next());
+      }
+      return null;
+    } else if (node instanceof UnaryExpression) {
+      UnaryExpression e = (UnaryExpression) node;
+      switch (e.getType()) {
+        case Token.NEG: {
+          Object value = toJavaObject(e.getOperand());
+          return value instanceof Number
+              ? -((Number) value).doubleValue() : Double.NaN;
+        }
+        case Token.POS: {
+          Object value = toJavaObject(e.getOperand());
+          return value instanceof Number
+              ? ((Number) value).doubleValue() : Double.NaN;
+        }
+        default: return null;
+      }
+    } else if (node instanceof ExpressionStatement) {
+      return toJavaObject(((ExpressionStatement) node).getExpression());
+    } else if (node instanceof ParenthesizedExpression) {
+      return toJavaObject(((ParenthesizedExpression) node).getExpression());
+    } else {
+      return null;
+    }
+  }
+
+  public static final class Lambda {
+    private final String source;
+
+    public Lambda(String source) {
+      assert source != null;
+      this.source = source;
+    }
+
+    public String getSource() { return source; }
+
+    @Override
+    public String toString() { return source; }
+
+    @Override
+    public boolean equals(Object o) {
+      return o instanceof Lambda && source.equals(((Lambda) o).source);
+    }
+
+    @Override
+    public int hashCode() { return source.hashCode(); }
+  }
+}
---
+++ tests/org/prebake/js/YSONTest.java
@@ -0,0 +1,227 @@
+package org.prebake.js;
+
+import org.prebake.core.MessageQueue;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+public class YSONTest extends TestCase {
+  public final void testFreeVars() throws Exception {
+    assertFreeVars("function () {}");
+    assertFreeVars("(function x() {})");
+    assertFreeVars("(function x(y) { return y == 1 ? 1 : y * x(y - 1); })");
+    assertFreeVars(
+        "(function x(y) { return y == 1 ? z : y * x(y - 1); })", "z");
+    assertFreeVars(
+        "(function x(y) { var z; return y == 1 ? z : y * x(y - 1); })");
+    assertFreeVars("(function x(y) { return e; })", "e");
+    assertFreeVars(
+        "(function x(y) { try { foo(); } catch (e) { return e; } })", "foo");
+    assertFreeVars(
+        "(function x(y) { try { foo(); } catch (e) {} return e; })",
+        "foo", "e");
+    assertFreeVars("this", "this");
+    assertFreeVars("eval", "eval");
+    assertFreeVars("function () { return function () { return x; } }", "x");
+  }
+
+  public final void testAstInfo() throws ParseException {
+    withCode("0").run();
+    withCode("x").rn("x").fn("x").run();
+    withCode("var x;").vsd("x").fn("x").run();
+    withCode("var x, y = z;").vsd("x", "y").rn("z").fn("x", "y", "z").run();
+    withCode("x(); { var x; }").vsd("x").rn("x").fn("x").run();
+    withCode("(function f(a) { var b; return a + b; })").run();
+    withCode("function f(a) { var b; return a + b; }").fn("f").vsd("f").run();
+    withCode("function f(a) { function g(b) { return a + b + c; }; return g; }")
+        .rn("c").fn("c", "f").vsd("f").run();
+    withCode("for (var i = 0, o; o = arr[i]; ++i) { foo(o); }").vsd("i", "o")
+        .rn("arr", "foo", "i", "o").fn("arr", "foo", "i", "o").run();
+    withCode(
+        "(function (arr) { for (var i = 0, o; o = arr[i]; ++i) { foo(o); } })()"
+        ).rn("foo").fn("foo").run();
+    withCode(""
+             + "(function a(b, c) {"
+             + " var d; function e(f) { var g; } return [a,b,c,d,e,f,g,h];"
+             + "})")
+        .rn("f", "g", "h").fn("f", "g", "h").run();
+    withCode("var a = function a(b, c) {}, d = e;").vsd("a", "d").rn("e")
+        .fn("a", "d", "e").run();
+    withCode("for (var k in o) { foo(o[k]); }").vsd("k").rn("foo", "k", "o")
+        .fn("foo", "k", "o").run();
+    withCode("for (k in o) { foo(o[k]); }").rn("foo", "k", "o")
+        .fn("foo", "k", "o").run();
+    withCode("try { f(e); } catch (e) { g(e); }").rn("e", "f", "g")
+        .fn("e", "f", "g").run();
+    withCode("try { f(h); } catch (e) { g(e); }").rn("f", "g", "h")
+        .fn("f", "g", "h").run();
+    withCode("try { f(h); } catch (e) { g(e); } finally { g(e); }")
+        .rn("e", "f", "g", "h").fn("e", "f", "g", "h").run();
+    withCode("try { f(h); } catch (e) { var x = g(e); }").vsd("x")
+        .rn("f", "g", "h").fn("f", "g", "h", "x").run();
+    withCode("try { f(h); } catch (e) { var e = g(e); }").vsd("e")
+        .rn("f", "g", "h").fn("e", "f", "g", "h").run();
+    withCode("try { f(h); } catch (e) { g(e); } finally { var e = g(e); }")
+        .vsd("e").rn("f", "g", "h", "e").fn("e", "f", "g", "h").run();
+    withCode(""
+             + "(function () {"
+             + "  return [this, that, arguments, argumentative, null,"
+             + "           undefined];"
+             + "})")
+        .rn("argumentative", "that", "undefined")
+        .fn("argumentative", "that", "undefined").run();
+    withCode("[this, that, arguments, argumentative, null, undefined]")
+        .rn("argumentative", "arguments", "that", "this", "undefined")
+        .fn("argumentative", "arguments", "that", "this", "undefined").run();
+    withCode("bar: while (1) { break bar; }").run();
+    withCode("bar: while (1) { continue bar; }").run();
+    withCode("bar: while (1) { break; }").run();
+    withCode("bar: while (1) { continue; }").run();
+    withCode("bar: do { break bar; } while (1)").run();
+    withCode("bar: do { continue bar; } while (1)").run();
+    withCode("bar: do { break; } while (1)").run();
+    withCode("bar: do { continue; } while (1)").run();
+    withCode("bar: for (;;) { break bar; }").run();
+    withCode("bar: for (;;) { continue bar; }").run();
+    withCode("bar: for (;;) { break; }").run();
+    withCode("bar: for (;;) { continue; }").run();
+    withCode("bar: switch (x) { default: break bar; }").rn("x").fn("x").run();
+    withCode("bar: switch (x) { default: break; }").rn("x").fn("x").run();
+    withCode("do { break; } while (1)").run();
+    withCode("do { continue; } while (1)").run();
+    withCode("(function () { return })()").run();
+    withCode("(function () { return bar })()").rn("bar").fn("bar").run();
+    withCode("eval('alert(\"foo\")')").rn("eval").fn("eval").run();
+    withCode("eval('alert(\"foo\")', bar)").rn("bar", "eval").fn("bar", "eval")
+        .run();
+  }
+
+  public final void testIsYSON() {
+    assertNotYson("foo bar", "missing ; before statement in foo bar");
+    assertYson("{ a: 1 }");
+    assertYson("[1,2,3]");
+    assertNotYson("[1,2,3,,]", "Not YSON: trailing commas or empty expression");
+    assertYson(" \"foo\" ");
+    assertYson(" 0.5 ");
+    assertYson(" -0.5 ");
+    assertYson(" +1e2 ");
+    assertYson(" [true, false, null] ");
+    assertNotYson("NaN", "Not YSON: NaN");
+    assertNotYson("this", "Disallowed free variables: this");
+    assertNotYson("undefined", "Not YSON: undefined");
+    assertYson("{ 'ok': function (x, y) { return x * y } }");
+    assertNotYson(
+        "{ 'ok': function (x) { return x * y } }",
+        "Disallowed free variables: y");
+    assertNotYson(
+        "{ foo: 0 }), ({ bar: 4 }", "Not YSON: ({foo: 0}) , ({bar: 4})");
+  }
+
+  public final void testFilter() throws Exception {
+    YSON yson;
+
+    yson = YSON.parseExpr("{ 'x': 'y', y: 2, z: { y: 4 } }");
+    yson = YSON.filterProperties(
+        yson, new Predicate<String>() {
+          public boolean apply(String key) { return !"y".equals(key); }
+        });
+    assertEquals("({'x': 'y', z: {y: 4}})", yson.toSource());
+
+    yson = YSON.parseExpr("{ 'x': 'y', y: 2, z: { y: 4 } }");
+    yson = YSON.filterProperties(
+        yson, new Predicate<String>() {
+          public boolean apply(String key) { return "y".equals(key); }
+        });
+    assertEquals("({y: 2})", yson.toSource());
+
+    yson = YSON.parseExpr("{ 'x': 'y', 2: 'two', '2.0': { y: 4 } }");
+    yson = YSON.filterProperties(
+        yson, new Predicate<String>() {
+          public boolean apply(String key) { return !"2".equals(key); }
+        });
+    assertEquals("({'x': 'y', '2.0': {y: 4}})", yson.toSource());
+  }
+
+  public final void testToJava() throws Exception {
+    assertEquals(
+        Arrays.asList("foo", "bar"),
+        YSON.parseExpr("['foo', 'bar']").toJavaObject());
+    assertEquals(Boolean.TRUE, YSON.parseExpr("true").toJavaObject());
+    assertEquals(Boolean.FALSE, YSON.parseExpr("false").toJavaObject());
+    assertEquals(-2.0, YSON.parseExpr("-2").toJavaObject());
+    assertEquals(null, YSON.parseExpr("null").toJavaObject());
+    Map<String, Object> golden = Maps.newLinkedHashMap();
+    golden.put("foo", "bar");
+    golden.put("bAz", ImmutableList.of("boo"));
+    golden.put("get", 3.0);
+    golden.put("3", null);
+    Map<?, ?> actual = (Map<?, ?>) YSON.parse(
+        "({ foo: 'bar', 'b\\x41z': ['boo'], get: 3, 3: null })")
+        .toJavaObject();
+    assertEquals(golden, actual);
+    assertEquals(  // iteration order preserved
+        Lists.newArrayList(golden.keySet()),
+        Lists.newArrayList(actual.keySet()));
+  }
+
+  private void assertFreeVars(String src, String... freeVars) throws Exception {
+    YSON yson = YSON.parse(src);
+    Joiner j = Joiner.on(", ");
+    assertEquals(j.join(freeVars), j.join(yson.getFreeNames()));
+  }
+
+  private Tester withCode(String js) { return new Tester(js); }
+
+  private static class Tester {
+    private final String code;
+    private final Set<String> vsd = Sets.newLinkedHashSet();
+    private final Set<String> rn = Sets.newLinkedHashSet();
+    private final Set<String> fn = Sets.newLinkedHashSet();
+
+    Tester(String code) { this.code = code; }
+
+    Tester vsd(String... names) {
+      vsd.addAll(Arrays.asList(names));
+      return this;
+    }
+    Tester rn(String... names) {
+      rn.addAll(Arrays.asList(names));
+      return this;
+    }
+    Tester fn(String... names) {
+      fn.addAll(Arrays.asList(names));
+      return this;
+    }
+    void run() throws ParseException {
+      YSON yson = YSON.parse(code);
+      assertEquals("vsd " + code, vsd, yson.getVarScopeDeclarations());
+      assertEquals("rn " + code, rn, yson.getRequiredNames());
+      assertEquals("fn " + code, fn, yson.getFreeNames());
+    }
+  }
+
+  private void assertNotYson(String src, String... msgs) {
+    MessageQueue mq = new MessageQueue();
+    assertFalse(src, YSON.isYSON(src, YSON.DEFAULT_YSON_ALLOWED, mq));
+    assertEquals(
+        Joiner.on("\n").join(msgs), Joiner.on("\n").join(mq.getMessages()));
+  }
+
+  private void assertYson(String src) {
+    MessageQueue mq = new MessageQueue();
+    if (!YSON.isYSON(src, YSON.DEFAULT_YSON_ALLOWED, mq)) {
+      fail(Joiner.on("\n").join(mq.getMessages()));
+    }
+  }
+}
