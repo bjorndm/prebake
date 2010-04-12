c6dbe93199f7b87471b1f124f941ee8d82fcb773
mikesamuel@gmail.com <mikesamuel@gmail.com@5c2138e2-2bf6-11df-8484-2f92f965d510>
2010-04-12 16:12:59 +0000

    added code to FileHashes to provide a list of paths matching a group of globs

--- src/org/prebake/fs/FileVersioner.java
+++ src/org/prebake/fs/FileVersioner.java
@@ -1,13 +1,17 @@
 package org.prebake.fs;
 
+import org.prebake.core.Glob;
 import org.prebake.core.Hash;
 
 import java.nio.file.Path;
 import java.util.Collection;
+import java.util.List;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
 public interface FileVersioner {
+  public Path getVersionRoot();
   public void getHashes(Collection<Path> paths, Hash.Builder out);
+  public List<Path> matching(List<Glob> globs);
 }
