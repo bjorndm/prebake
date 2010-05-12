24ace876f50acf3ed6c27c7040066555b940b355
mikesamuel <mikesamuel@5c2138e2-2bf6-11df-8484-2f92f965d510>
2010-05-12 18:49:59 +0000

    debugged building on Linux

--- src/org/prebake/service/Prebakery.java
+++ src/org/prebake/service/Prebakery.java
@@ -246,9 +246,11 @@ public abstract class Prebakery implements Closeable {
     Path dir = clientRoot.resolve(fs.getPath(FileNames.DIR));
     if (!dir.exists()) {
       dir.createDirectory(FilePerms.perms(config.getUmask(), true));
-      DosFileAttributeView dosAttrs = dir.getFileAttributeView(
-          DosFileAttributeView.class);
-      if (dosAttrs != null) { dosAttrs.setHidden(true); }
+      if ("/".equals(fs.getSeparator())) {
+        DosFileAttributeView dosAttrs = dir.getFileAttributeView(
+            DosFileAttributeView.class);
+        if (dosAttrs != null) { dosAttrs.setHidden(true); }
+      }
     }
     Path cmdlineFile = dir.resolve(fs.getPath(FileNames.CMD_LINE));
     Path portFile = dir.resolve(fs.getPath(FileNames.PORT));
