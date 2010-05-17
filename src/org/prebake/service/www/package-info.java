da0902bde31036d403e691b215fadaf72ae1c84c
mikesamuel@gmail.com <mikesamuel@gmail.com@5c2138e2-2bf6-11df-8484-2f92f965d510>
2010-05-17 17:57:35 +0000

    First cut at URL space for the servlet.

---
+++ src/org/prebake/service/www/package-info.java
@@ -0,0 +1,50 @@
+// Copyright 2010, Mike Samuel
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * An HTTP servlet that serves status information and documentation for tools
+ * and plans, and dependency information for products.
+ * <p>
+ * It also provides a POST interface for continuous integration tools, and a
+ * way for products to easily deploy HTML reports from tools like
+ * findbugs, xunits, javadoc, doxygen, etc.
+ * <p>
+ * The HTTP servlet listens on the
+ * {@link org.prebake.service.Config#getWwwPort www port} and services
+ * requests to the following URL space:
+ * <pre>
+ * /
+ *   index.html                             Starting page with links
+ *   plans/
+ *     index.html                           Plan graph and list of products
+ *     plans.json                           JSON formatted plan summary.
+ *     &lt;product-name<sub>0</sub>&gt;                      HTML product docs.
+ *     &lt;product-name<sub>1</sub>&gt;
+ *     ...
+ *     &lt;product-name<sub>n</sub>&gt;
+ *   tools/
+ *     index.html                           List of tools.
+ *     tools.json                           JSON formatted tool summary.
+ *     &lt;tool-name<sub>0</sub>&gt;                         HTML tool docs.
+ *     &lt;tool-name<sub>1</sub>&gt;
+ *     ...
+ *     &lt;tool-name<sub>n</sub>&gt;
+ *   mirror/
+ *     &lt;mirror of tree under prebake-www&gt;   Generated client reports.
+ *   prebake-api/
+ *     do                                   POST only API.
+ * </pre>
+ */
+@javax.annotation.ParametersAreNonnullByDefault
+package org.prebake.service.www;
