b9ba5b4b636b9c6749767dd77491b29d0d39007f
mikesamuel@gmail.com <mikesamuel@gmail.com@5c2138e2-2bf6-11df-8484-2f92f965d510>
2010-06-07 04:57:43 +0000

    added recent event log to plan/index.html in the servlet

---
+++ src/org/prebake/service/www/www-files/plan-graph.js
@@ -0,0 +1,87 @@
+// Supporting JS for /plan/index.html
+// This is loosely adapted from third_party/foograph/test.html
+
+(function() {
+  // Styles used for different kinds of nodes in the draw function below.
+  // Dark green border around green.
+  var staleNode = new VertexStyle('ellipse', 80, 40, '#cccccc', '#333333', true);
+  // Dark grey border around grey.
+  var upToDateNode = new VertexStyle('ellipse', 80, 40, '#aaffaa', '#004400', true);
+
+  function noop() {}
+
+  // Send an XMLHttpRequest to plan.json for graph data and layout the graph
+  // when it arrives and refetches after some time.
+  var lastJsonDrawn;
+  function requestGraph() {
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+      if (req.readyState === 4) {
+        var json = req.responseText;
+        req.onreadystatechange = noop;  // Work around GC problems.
+        setTimeout(requestGraph, 2500 /*ms*/);
+        if (req.status === 200) {
+          if (lastJsonDrawn !== json) {
+            lastJsonDrawn = json;
+            drawGraph(JSON.parse(json));
+          }
+        }
+      }
+    };
+    // Assumes current directory is in the plan directory
+    req.open('GET', 'plan.json', true);
+    req.send();
+  }
+
+  function scrollerTo(nodeName) {
+    return function () { window.location = '#detail:' + nodeName; };
+  }
+
+  var canvasCont = document.getElementById('plan-graph-cont');
+  var width = canvasCont.offsetWidth - 4;
+  var height = canvasCont.offsetHeight - 4;
+
+  // Draws a graph given JSON of the form fetchable from plan.json.
+  function drawGraph(graphJson) {
+    Math.resetRandom();
+
+    var g = new Graph('Plan Graph', true/*directed*/);
+    var vertices = {};
+    for (var nodeName in graphJson.graph) {
+      var nodeInfo = graphJson.graph[nodeName];
+      var style = nodeInfo.upToDate ? upToDateNode : staleNode;
+      var vertex = vertices[nodeName] = new Vertex(nodeName, -1, -1, style);
+      g.insertVertex(vertex);
+    }
+
+    for (var nodeName in graphJson.graph) {
+      var adjacentNodes = graphJson.graph[nodeName].requires;
+      var startVertex = vertices[nodeName];
+      for (var i = 0, n = adjacentNodes.length; i < n; ++i) {
+        var endVertex = vertices[adjacentNodes[i]];
+        var label = null;  // TODO: label with glob intersection.
+        g.insertEdge(label, 3/*weight*/, startVertex, endVertex, null/*style*/);
+      }
+    }
+
+    // Layout the graph
+    new KamadaKawaiVertexLayout(width, height).layout(g);
+    new ForceDirectedVertexLayout(width, height, 400, false/*re-randomize*/)
+        .layout(g);
+
+    // Set up the canvas
+    var canvasCanvas = document.getElementById('plan-graph');
+    canvasCanvas.style.width = width + 'px';
+    canvasCanvas.style.height = height + 'px';
+    canvasCanvas.width = width;
+    canvasCanvas.height = height;
+
+    // Draw the graph
+    var canvas = new canvasWrapper(canvasCanvas);
+    canvas.clear();
+    g.plot(canvas);
+    canvas.paint();
+  }
+
+  requestGraph();
+})();
